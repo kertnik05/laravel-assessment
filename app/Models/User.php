47d143e472126ab829ce99c2947977350c4cd4e2
Nikko Lumahang <kertnik@gmail.com>
2020-12-09 23:41:27 +0800

    added middle initial accessor

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     {
         return $this->photo;
     }
+
+    public function getMiddleinitialAttribute()
+    {
+        return $this->middlename[0].'.';
+    }
 }
